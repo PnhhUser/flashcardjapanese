c8c04d245705a8e6231b7e49ef6bbcd90245ae1a
PnhhUser <pnhh240300@gmail.com>
2026-05-05 01:31:45 +0700

    up

--- src/core/services/japanese.ts
+++ src/core/services/japanese.ts
@@ -3,16 +3,13 @@ import type { ApiResponse } from "../model/api";
 import type { Japanese } from "../model/japanese";
 
 export const getHiragana = async () => {
-  const res =
-    await apiClient.get<ApiResponse<Japanese[]>>("/japanese/hiragana");
+  const res = await apiClient.get<ApiResponse<Japanese[]>>("/jp/hiragana");
 
   return res.data;
 };
 
 export const getHiraganaVocabulary = async () => {
-  const res = await apiClient.get<ApiResponse<Japanese[]>>(
-    "/japanese/vocabulary",
-  );
+  const res = await apiClient.get<ApiResponse<Japanese[]>>("/jp/vocabulary");
 
   return res.data;
 };
